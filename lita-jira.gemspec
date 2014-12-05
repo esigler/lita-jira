7000f1593bcdb0d0d57ccdfd7919265bfea6af24
Eric Sigler <me@esigler.com>
2014-12-04 16:25:39 -0800

    Prep work for updates

--- lita-jira.gemspec
+++ lita-jira.gemspec
@@ -14,12 +14,12 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(/^(test|spec|features)\//)
   spec.require_paths = ['lib']
 
-  spec.add_runtime_dependency 'lita', '>= 3.0'
+  spec.add_runtime_dependency 'lita', '>= 4.0'
   spec.add_runtime_dependency 'jira-ruby', '>= 0.1.8'
 
   spec.add_development_dependency 'bundler', '~> 1.3'
   spec.add_development_dependency 'rake'
-  spec.add_development_dependency 'rspec', '>= 3.0.0.beta2'
+  spec.add_development_dependency 'rspec', '>= 3.0.0'
   spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'coveralls'
   spec.add_development_dependency 'rubocop'
--- spec/lita/handlers/jira_spec.rb
+++ spec/lita/handlers/jira_spec.rb
@@ -14,33 +14,35 @@ describe Lita::Handlers::Jira, lita_handler: true do
       receive(:fetch_issue).with(key).and_return(issue)
   end
 
-  it { routes_command('todo some text').to(:todo) }
-  it { routes_command('jira issue assignee ABC-123').to(:issue_assignee_list) }
-  it { routes_command('jira issue assignee ABC-123 foo@example.com').to(:issue_assignee_set) }
-  it { routes_command('jira issue attachments ABC-123').to(:issue_attachments_list) }
-  it { routes_command('jira issue attachments ABC-123 http://example.com').to(:issue_attachments_set) }
-  it { routes_command('jira issue comments ABC-123').to(:issue_comments_list) }
-  it { routes_command('jira issue comments ABC-123 Some text').to(:issue_comments_add) }
-  it { routes_command('jira issue details ABC-123').to(:issue_details) }
-  it { routes_command('jira issue issuetype ABC-123').to(:issue_issuetype_list) }
-  it { routes_command('jira issue issuetype ABC-123 4').to(:issue_issuetype_set) }
-  it { routes_command('jira issue new ABC some:thing').to(:issue_new) }
-  it { routes_command('jira issue notify ABC-123').to(:issue_notify_list) }
-  it { routes_command('jira issue notify ABC-123 foo@example.com').to(:issue_notify_set) }
-  it { routes_command('jira issue priority ABC-123').to(:issue_priority_list) }
-  it { routes_command('jira issue priority ABC-123 9').to(:issue_priority_set) }
-  it { routes_command('jira issue summary ABC-123').to(:issue_summary_list) }
-  it { routes_command('jira issue summary ABC-123 Some text!').to(:issue_summary_set) }
-  it { routes_command('jira issue watchers ABC-123').to(:issue_watchers_list) }
-  it { routes_command('jira issue watchers ABC-123 foo@example.com').to(:issue_watchers_set) }
-  it { routes_command('jira issuetype list ABC').to(:issuetype_list) }
-  it { routes_command('jira search Some text').to(:search_full) }
-  it { routes_command('jira search ABC Some text').to(:search_project) }
-  it { routes_command('jira identify foo@example.com').to(:identify) }
-  it { routes_command('jira forget').to(:forget) }
-  it { routes_command('jira whoami').to(:whoami) }
-  it { routes_command('jira default project ABC').to(:default_project) }
-  it { routes_command('jira default priority 9').to(:default_priority) }
+  it do
+    is_expected.to route_command('todo some text').to(:todo)
+    is_expected.to route_command('jira issue assignee ABC-123').to(:issue_assignee_list)
+    is_expected.to route_command('jira issue assignee ABC-123 foo@example.com').to(:issue_assignee_set)
+    is_expected.to route_command('jira issue attachments ABC-123').to(:issue_attachments_list)
+    is_expected.to route_command('jira issue attachments ABC-123 http://example.com').to(:issue_attachments_set)
+    is_expected.to route_command('jira issue comments ABC-123').to(:issue_comments_list)
+    is_expected.to route_command('jira issue comments ABC-123 Some text').to(:issue_comments_add)
+    is_expected.to route_command('jira issue details ABC-123').to(:issue_details)
+    is_expected.to route_command('jira issue issuetype ABC-123').to(:issue_issuetype_list)
+    is_expected.to route_command('jira issue issuetype ABC-123 4').to(:issue_issuetype_set)
+    is_expected.to route_command('jira issue new ABC some:thing').to(:issue_new)
+    is_expected.to route_command('jira issue notify ABC-123').to(:issue_notify_list)
+    is_expected.to route_command('jira issue notify ABC-123 foo@example.com').to(:issue_notify_set)
+    is_expected.to route_command('jira issue priority ABC-123').to(:issue_priority_list)
+    is_expected.to route_command('jira issue priority ABC-123 9').to(:issue_priority_set)
+    is_expected.to route_command('jira issue summary ABC-123').to(:issue_summary_list)
+    is_expected.to route_command('jira issue summary ABC-123 Some text!').to(:issue_summary_set)
+    is_expected.to route_command('jira issue watchers ABC-123').to(:issue_watchers_list)
+    is_expected.to route_command('jira issue watchers ABC-123 foo@example.com').to(:issue_watchers_set)
+    is_expected.to route_command('jira issuetype list ABC').to(:issuetype_list)
+    is_expected.to route_command('jira search Some text').to(:search_full)
+    is_expected.to route_command('jira search ABC Some text').to(:search_project)
+    is_expected.to route_command('jira identify foo@example.com').to(:identify)
+    is_expected.to route_command('jira forget').to(:forget)
+    is_expected.to route_command('jira whoami').to(:whoami)
+    is_expected.to route_command('jira default project ABC').to(:default_project)
+    is_expected.to route_command('jira default priority 9').to(:default_priority)
+  end
 
   describe '.default_config' do
     it 'sets username to nil' do
