b08e026d01acab6167d278a317d6533758e88b1f
Eric Sigler <me@esigler.com>
2017-02-03 12:34:47 -0800

    Pull coveralls, fix issue with test ordering

--- lita-jira.gemspec
+++ lita-jira.gemspec
@@ -18,7 +18,6 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'jira-ruby'
 
   spec.add_development_dependency 'bundler'
-  spec.add_development_dependency 'coveralls'
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rspec'
   spec.add_development_dependency 'rubocop'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'simplecov'
-require 'coveralls'
 SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
+  SimpleCov::Formatter::HTMLFormatter
 ]
 SimpleCov.start { add_filter '/spec/' }
 
@@ -10,6 +8,24 @@ require 'lita-jira'
 require 'lita/rspec'
 
 Lita.version_3_compatibility_mode = false
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # TODO: Enable
+  # config.mock_with :rspec do |mocks|
+  #   mocks.verify_partial_doubles = true
+  # end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.order = :random
+
+  Kernel.srand config.seed
+end
 
 def grab_request(result)
   allow(JIRA::Client).to receive(:new) { result }
